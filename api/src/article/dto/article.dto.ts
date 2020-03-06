b364528940e11ce6e7244ffba9550b167d1cfb5c
barbara <barbara_carme@yahoo.fr>
2020-03-06 16:05:15 +0100

    swagger

--- api/src/article/dto/article.dto.ts
+++ api/src/article/dto/article.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ArticleDto {
+    @ApiProperty()
     readonly title: string;
+
+    @ApiProperty()
     readonly author: string;
+
+    @ApiProperty()
     readonly description: string;
+
+    @ApiProperty()
     readonly content: string;
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -5,17 +5,26 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { join } from 'path';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const options = new DocumentBuilder()
+  .setTitle('Article exemple')
+  .setDescription('The articles API description')
+  .setVersion('1.0')
+  .addTag('articles')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
-  /*app.useStaticAssets({
-    root: join(__dirname, '..', 'client/dist/client'),
-    prefix: '/',
-  });*/
-  app.setGlobalPrefix('/api');
+  //app.setGlobalPrefix('/api');
+
+
   await app.listen(3000);
 }
 bootstrap();
